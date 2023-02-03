ce8df4850105adb9f98ef378e1090d035d8894c3
Sengah Choi <cntjr3@naver.com>
2023-02-03 16:31:28 +0900

    2/3 16:31_최성아_signup 마무리 중

--- cinema/public/member/signup.js
+++ cinema/public/member/signup.js
@@ -10,7 +10,18 @@ let userArray = [
 			]
 
 /* 등록되어 있는 영화 목록 */
-
+let movieList =  [
+				 {name : '더 퍼스트 슬램덩크', img : 'slam_dunk', category : '애니메이션', age : 12, star : 9.24},
+				 {name : '장화신은 고양이', img : 'PussInBoots', category : '애니메이션', age : 0, star : 8.41},
+				 {name : '캐리의 슈퍼콜라', img : 'CarrieAndSuperKola', category : '애니메이션', age : 0, star : 8.69},
+				 {name : '교섭', img : 'ThePointMan', category : '드라마', age : 12, star : 6.30},
+				 {name : '아바타', img : 'avatar', category : '액션,모험,SF,스릴러', age : 12, star : 8.83},
+				 {name : '메간', img : 'M3GAN', category : '공포,스릴러,코미디', age : 15, star : 7.34},
+				 {name : '유령', img : 'phantom', category : '액션', age : 15, star : 6.75},,
+				 {name : '상견니', img : 'SomedayOrOneDay', category : '드라마,판타지,멜로/로맨스', age : 15, star : 7.83},
+				 {name : '영웅', img : 'Hero', category : '드라마,뮤지컬', age : 12, star : 8.41},
+				 {name : '오늘밤, 세계에서 이 사랑이 사라진다해도', img : 'EvenIfThisLoveDissappear', category : '멜로/로맨스', age : 12, star : 7.99}
+				 ]
 
 
 function singupbtn(){ // f s
