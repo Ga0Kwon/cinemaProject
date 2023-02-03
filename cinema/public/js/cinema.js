7435380aa92ee0bfe5017aefa53d2de2293f3bf5
Chunma <acttesty44@naver.com>
2023-02-03 17:16:13 +0900

    2023/02/03 17:16

--- cinema/public/js/cinema.js
+++ cinema/public/js/cinema.js
@@ -20,7 +20,7 @@ function poster_print(){
 		html += `<div class="item">
 					 <img src="img/mainimg/${o.img}" onclick="introduce(${i})">
 
-					<a href="#"><img src="img/mainimg/${ o.img }"></a>
+					
 					<div class="posterinfo">
 						<div class="itemname">${ o.title }<span class="badge rounded-pill grage">${ o.grade }</span></div>
 					</div>
